361b0f3a64cf1b99a1e5a07d5acf2bebffd64b08
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2024-02-16 21:51:55 +0000

    add walking animation

--- player/player.gd
+++ player/player.gd
@@ -5,6 +5,10 @@ var speed = 400  # move speed in pixels/sec
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
 	var move_input = Input.get_vector("left", "right", "up", "down")
+	if move_input.is_zero_approx():
+		$BodySpriteAnim.play("idle")
+	else:
+		$BodySpriteAnim.play("walking")
 	velocity = move_input * speed
 	move_and_slide()
 
