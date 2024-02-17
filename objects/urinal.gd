a47cb6a492284c919103cdae0bebd73794938a07
James Wild <wild.james343@gmail.com>
2024-02-17 15:51:50 +0000

    Player has finite piss

--- objects/urinal.gd
+++ objects/urinal.gd
@@ -1,4 +1,24 @@
 extends StaticBody2D
 
+var filled_volume = 0
+var max_volume = 100
+
+# This will be added per frame, as the pissed_on function is called by things in the physics process
+var fill_frame_delta = 1 
+
+# Four sprites, from empty to full
+var sprite_change_thresholds = [0, 25, 50, 75]
+var sprites = []
+
 func pissed_on() -> void:
-	$AnimatedSprite2D.play("pissing")
+	if filled_volume < max_volume:
+		filled_volume += fill_frame_delta
+
+	$AnimatedSprite2D.animation = "pissing"
+
+	var i = 0
+	for threshold in sprite_change_thresholds:
+		if filled_volume > threshold:
+			$AnimatedSprite2D.frame = i
+		i += 1
+
--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,13 @@ extends CharacterBody2D
 var speed = 400  # move speed in pixels/sec
 var is_pissing = false
 
+var current_piss_volume = 100
+var max_piss_volume = 200
+
+# per physics frame
+var pissing_delta = 2 
+var piss_buildup_delta = 1
+
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
 	var move_input = Input.get_vector("left", "right", "up", "down")
@@ -21,20 +28,32 @@ func _physics_process(delta):
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("piss"):
 		start_piss()
-		
+
 	if event.is_action_released("piss"):
 		stop_piss()
 
 func start_piss() -> void:
-	$PissParticles.emitting = true
-	is_pissing = true
+	if current_piss_volume > 0:
+		$PissParticles.emitting = true
+		is_pissing = true
 
 func stop_piss() -> void:
 	$PissParticles.emitting = false
 	is_pissing = false
 
 func check_piss() -> void:
-	if $PissRaycast.is_colliding():
-		var obj : Object = $PissRaycast.get_collider()
-		if obj.has_method("pissed_on"):
-			obj.pissed_on()
+	if current_piss_volume > 0:
+		current_piss_volume -= pissing_delta
+
+		# Check for piss targets
+		if $PissRaycast.is_colliding():
+			var obj : Object = $PissRaycast.get_collider()
+			if obj.has_method("pissed_on"):
+				obj.pissed_on()
+
+	else:
+		stop_piss()
+		empty_bladder()
+
+func empty_bladder() -> void:
+	print("Bladder is empty!")
