60d47329fa768c85b4f0e4d547f163b3223a8eac
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2024-02-17 20:20:58 +0000

    refactor: make bar a standard level

--- levels/level_manager.gd
+++ levels/level_manager.gd
@@ -1,10 +1,14 @@
 extends Node
 
+
 func change_to(next_level: PackedScene) -> void:
+	assert(next_level, "level node is missing a next level scene!")
 	call_deferred("_do_the_change", next_level)
+
 
 func _do_the_change(next_level: PackedScene) -> void:
 	get_tree().change_scene_to_packed(next_level)
+
 
 func restart() -> void:
 	get_tree().reload_current_scene()
