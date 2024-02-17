ac634b012b7da48be91dbe587ad683a3acb5fff9
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2024-02-17 17:23:58 +0000

    fix: stop error on scene change

--- levels/level_manager.gd
+++ levels/level_manager.gd
@@ -1,4 +1,7 @@
 extends Node
 
 func change_to(next_level: PackedScene) -> void:
+	call_deferred("_do_the_change", next_level)
+
+func _do_the_change(next_level: PackedScene) -> void:
 	get_tree().change_scene_to_packed(next_level)
