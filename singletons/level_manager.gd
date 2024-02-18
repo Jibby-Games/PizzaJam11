c33e0c698cb7725e5c421d0f8d2c50c2c40999ee
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2024-02-18 21:47:28 +0000

    feat: show level names

--- singletons/level_manager.gd
+++ singletons/level_manager.gd
@@ -1,5 +1,6 @@
 extends Node
 
+
 var levels := [
 	preload("res://levels/bar.tscn"),
 	preload("res://levels/level1.tscn"),
@@ -8,7 +9,18 @@ var levels := [
 	preload("res://levels/level4.tscn")
 ]
 
+var level_names := [
+	"The Aperitif",
+	"The Ideal",
+	"Awaiting Restoration",
+	"Companionship",
+	"Complications",
+]
+
 var current_level := 0
+
+func _ready() -> void:
+	assert(levels.size() == level_names.size(), "Not all levels have names in level_names!")
 
 func load_next_level() -> void:
 	current_level += 1
@@ -16,6 +28,7 @@ func load_next_level() -> void:
 		print_debug("reached end of levels! you win?")
 		return
 	change_to(levels[current_level])
+	UI.set_level_name(level_names[current_level])
 
 
 func change_to(next_level: PackedScene) -> void:
