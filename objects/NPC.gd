815164988025b7f8c4299f4e3b24a548fc395ff2
wildjames <james.wild@techcitylabs.com>
2024-02-18 21:55:31 +0000

    multiple npc lines

---
+++ objects/NPC.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+
+@export_multiline var entered_text_array: PackedStringArray = ["Occupied!", "Isn't that a bit friendly?"]
+@export var entered_text_random: bool = true
+
+@export_multiline var exited_text_array: PackedStringArray = ["*sigh*", "Thanks"]
+@export var exited_text_random: bool = true
--- objects/NPCProximity.gd
+++ objects/NPCProximity.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-
 var DIALOGUE_SCENE = preload("res://objects/DialogueBox.tscn")
-
 var dialogue_instance
+
+var num_entered = 0
+var num_exited = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,9 +29,29 @@ func set_text(text: String) -> void:
 	get_parent().get_parent().add_child(dialogue_instance)
 
 
+func get_rand_element(array: PackedStringArray) -> String:
+	return array[randi() % array.size()]
+
+
 func _on_body_entered(body):
-	set_text("I see you!")
+	var options = get_parent().entered_text_array
+	var this_text = ""
+	if get_parent().entered_text_random:
+		this_text = get_rand_element(options)
+	else:
+		this_text = options[num_entered % len(options)]
+
+	set_text(this_text)
+	num_entered += 1
 
 
 func _on_body_exited(body):
-	set_text("Where did you go?")
+	var options = get_parent().exited_text_array
+	var this_text = ""
+	if get_parent().exited_text_random:
+		this_text = get_rand_element(options)
+	else:
+		this_text = options[num_exited % len(options)]
+
+	set_text(this_text)
+	num_exited += 1
