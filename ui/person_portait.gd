0731fc7819dad726007bbdf591037081489d3c6b
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2024-02-20 20:02:57 +0000

    feat: add sweat particles to portraits

---
+++ ui/person_portait.gd
@@ -0,0 +1,10 @@
+extends TextureRect
+
+var sweat_level := 0.0
+var max_particles := 100
+
+## Sweat intensity between 0.0 and 1.0, but can go higher (not too much or it looks stupid)
+func set_sweat_level(amount: float) -> void:
+	sweat_level = amount
+	$SweatParticles.emitting = (not is_zero_approx(sweat_level))
+	$SweatParticles.amount = sweat_level * max_particles
