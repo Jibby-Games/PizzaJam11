f31db45c008c23b848fc46cd8345a4c71f6d5994
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2024-02-21 23:20:10 +0000

    fix: use amount_ratio for particles to fix effect

--- ui/person_portait.gd
+++ ui/person_portait.gd
@@ -1,11 +1,9 @@
 extends Control
 
 var sweat_level := 0.0
-var max_particles := 100
 
 ## Sweat intensity between 0.0 and 1.0, but can go higher (not too much or it looks stupid)
 func set_sweat_level(amount: float) -> void:
 	sweat_level = amount
 	$SweatParticles.emitting = (not is_zero_approx(sweat_level))
-	var particles := ((2 ** sweat_level) - 1)  * max_particles
-	$SweatParticles.amount = clampi(particles, 1, 1000000)
+	$SweatParticles.amount_ratio = ((2 ** sweat_level) - 1)
