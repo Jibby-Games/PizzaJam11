0fa9fc10117b1c1d1345c4b972c04826593204d7
James Wild <wild.james343@gmail.com>
2024-02-24 17:19:08 +0000

    Finishing the awkward scene loads the next level

--- levels/PartyGame/arrival.gd
+++ levels/PartyGame/arrival.gd
@@ -1,6 +1,9 @@
 extends Level
 
 
+var DOOR_TRIGGER = preload("res://objects/door_trigger.tscn")
+
+
 func _ready() -> void:
 	UI.start_cinematic()
 	$AnimationPlayer.play("intro")
@@ -10,3 +13,7 @@ func _ready() -> void:
 
 func _on_taxi_awkward_trigger_trigger_done() -> void:
 	$AnimationPlayer.play("taxi_drive_off")
+
+
+func _on_enter_house_awkward_trigger_trigger_done():
+	Levels.load_next_level()
